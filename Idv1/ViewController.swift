e84710170672b45f46109a448bb282dff94d9dd8
Chetan Mahajan <chetan_mahajan@gwu.edu>
2018-01-27 15:26:04 -0500

    Placed the Cards and make them Flip

--- Idv1/ViewController.swift
+++ Idv1/ViewController.swift
@@ -9,7 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    var flipCount = 0 {
+        didSet {
+            flipCountLabel.text = "Flips: \(flipCount)"
+        }
+    }
+    
+    var emojiChoices : Array<String> = ["👻","⌚️","🌂","🎩"]
 
+    @IBOutlet weak var flipCountLabel: UILabel!
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1
+        if let cardNumber = cardButtons.index(of: sender) {
+                flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
+        }else {
+          print("Chosen card was not in card numbers.")
+        }
+        
+        //print("cardNumber = \(cardNumber)")
+    }
+    
+    func flipCard(withEmoji emoji: String, on button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+        }else {
+            button.setTitle(emoji, for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
